ee9afbc7fffd4e4163831342706e2fdfe3d78c7e
younes.r78@gmail.com <younes.r78@gmail.com>
2015-09-21 22:43:42 +0200

    Premier decoupage en fonction -> sans objet sac

---
+++ src/problemeSacADos/Sac.java
@@ -0,0 +1,20 @@
+package problemeSacADos;
+/**
+ * Class permettant de caracteriser un objet sac
+ */
+public class Sac {
+	//attributs d'un sac
+	
+	//Objets du sac référencer par des lettres
+	char[] objets;
+	//Poids de chacun des objets
+	int[] poids;
+	//valeur de chacun des objets
+	int[] Valeurs;
+	//Poids maximum
+	int maxPoid;
+	
+	public Sac(){
+		
+	}
+}
--- src/problemeSacADos/SacADos.java
+++ src/problemeSacADos/SacADos.java
@@ -2,9 +2,6 @@ package problemeSacADos;
 
 import java.util.ArrayList;
 
-
-
-
 public class SacADos {
 	
 	public static void main(String[] args) {
@@ -15,43 +12,75 @@ public class SacADos {
 	
 	/**
 	 * Methode de resolution du probleme de sac à dos (test)
-	 * @author YR
 	 *@return une liste contenant une combinaison optimal
 	 */
-	public static ArrayList<Character> progDynamique(int maxPoid){
+	public static ArrayList<Character> progDynamique(int maxPoids){
 		//variables utilisées
 		char[] tabObjet = {'A','B','C','D','E','F','G','H'};
 		int[] tabValeurs = {5, 8, 14, 6, 13, 17, 10, 4};
 		int[] tabPoids = {2, 3,  5, 2,  4,  6,  3, 1};
 		int a = tabObjet.length;
-		int[][] tabTraitement = new int[a][maxPoid + 1];
+		int[][] tabTraitement = new int[a][maxPoids + 1];
 		ArrayList<Character> combinaison = new ArrayList<Character>()  ;
 		
+		//debut du traitement remplissage de la premiere ligne
+		tabTraitement = initTabTraitement(tabTraitement,tabPoids,tabValeurs,maxPoids);
+		//remplissages des lignes suivantes
+		tabTraitement = traitementObjet(tabTraitement, tabPoids, tabValeurs, maxPoids);
+		
+		//recuperation de la combinaison
+		combinaison = getCombinaison(tabTraitement, tabPoids, tabObjet);
+		return combinaison;
+	}
+	/**
+	 * initialisation du tableau de traitement de l'algorithme de resolution du sac à dos
+	 * @param tabIni
+	 * @param tabPoids
+	 * @param tabValeurs
+	 * @param maxPoids
+	 * @return
+	 */
+	public static int[][] initTabTraitement(int[][] tabIni,int[] tabPoids,int[] tabValeurs,int maxPoids){
 		//debut du traitement 
 		//remplissage de la premiere ligne
 		int j = 0;
-		while(j < maxPoid +1) {
+		while(j < maxPoids +1) {
 			if (tabPoids[0] > j)
-				tabTraitement[0][j] = 0;
+				tabIni[0][j] = 0;
 			else
-				tabTraitement[0][j] = tabValeurs[0];
+				tabIni[0][j] = tabValeurs[0];
 			j++;
-		}
-			//reutilisation de j pour l'indice
-			j--;
-		//remplissages des lignes suivantes
+			}
+		return tabIni;
+	}
+	/**
+	 * Traitement des objet (chaque ligne de la matrice correspond à un objet)
+	 * @param tabTraitement
+	 * @param tabPoids
+	 * @param tabValeurs
+	 * @param maxPoids
+	 * @return
+	 */
+	public static int[][] traitementObjet(int[][] tabTraitement,int[] tabPoids,int[] tabValeurs ,int maxPoids){
 		for (int i = 1 ; i < tabPoids.length; i++){
-			for(int k = 0;k<=maxPoid ;k++){
+			for(int k = 0;k<=maxPoids ;k++){
 				if (tabPoids[i] > k)
 					tabTraitement[i][k] = tabTraitement[i-1][k] ;
 				else 
 					tabTraitement[i][k] = Math.max(tabTraitement[i-1][k],tabTraitement[i-1][k - tabPoids[i]] + tabValeurs[i]);
-					//System.out.println( Math.max(tabTraitement[i-1][j],tabTraitement[i-1][j - tabPoids[i]] + tabValeurs[i]));
 			}
 		}
-		
-		
-		//recuperation de la combinaison
+		return tabTraitement;
+	}
+	/**
+	 * Recuperation de la combinaison optimale
+	 * @param tabTraitement
+	 * @param tabPoids
+	 * @param tabObjet
+	 * @return
+	 */
+	public static ArrayList<Character> getCombinaison(int[][] tabTraitement,int[] tabPoids,char[] tabObjet){
+		ArrayList<Character> combinaison = new ArrayList<Character>();
 		int x = tabTraitement.length - 1 ;
 		int y = tabTraitement[0].length - 1 ;
 		while(tabTraitement[x][y] == tabTraitement[x][y - 1]){
@@ -64,13 +93,12 @@ public class SacADos {
 			y -= tabPoids[x];
 			if (y >= 0){
 				combinaison.add(tabObjet[x]);
-				//System.out.println(tabObjet[x]);
 			}
 			x--;
 		}	
-		//System.out.println(testString(tabTraitement));
 		return combinaison;
 	}
+	
 	public static String testString(int tab[][]){
 		String retour ="";
 		for (int i = 0 ; i < tab.length;i++){
